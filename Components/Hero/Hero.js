04729cf91f5e8a6f4ec535071618d89cab9aeb9d
albamartinez128 <albamartinez128@gmail.com>
2023-06-26 10:36:09 +0200

    Estilos-Hero-Animación

--- Components/Hero/Hero.js
+++ Components/Hero/Hero.js
@@ -7,11 +7,38 @@ const template = () => {
   <img src="./img/alba.jpeg" alt="imagen hero">
   </div>
   <div class="hero_texto">
+  <div class="texto_bienvenidos">
+    <h3 class="span loader">
+    <span class="m">W</span>
+    <span class="m">E</span>
+    <span class="m">L</span>
+    <span class="m">C</span>
+    <span class="m">O</span>
+    <span class="m">M</span>
+    <span class="m">E</span>
+    <span class="m">&nbsp;</span>
+    <span class="m">T</span>
+    <span class="m">O</span>
+    <span class="m">&nbsp;</span>
+  
+    <span class="m">M</span>
+    <span class="m">Y</span>
+    <span class="m">&nbsp;</span>
+  
+    <span class="m">P</span>
+    <span class="m">O</span>
+    <span class="m">R</span>
+    <span class="m">T</span>
+    <span class="m">F</span>
+    <span class="m">O</span>
+    <span class="m">L</span>
+    <span class="m">I</span>
+    <span class="m">O</span>
+  </h3>
+    </div>
     <h2>ALBA</h2>
     <h3>MARTINEZ</h3>
-    <div class="texto_bienvenidos">
-      <h4>WELCOME TO MY PORTFOLIO</h4>
-    </div>
+    
     <div class="iconos_rrss"></div>
   </div>
 </div>
